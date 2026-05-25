5be6cedd57a154c079a4e5eeab760f23bd8d80f2
Ashutosh Tiwari <ashutoshhtiwari07@gmail.com>
2026-05-25 21:09:09 +0530

    Atom family

--- week7/7.4/src/App2.jsx
+++ week7/7.4/src/App2.jsx
@@ -1,14 +1,27 @@
 // ATOM FAMILY
 
 import './App.css'
-import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil';
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { todosAtomFamily, todosAtom } from './atoms2';
 
 function App() {
   return <RecoilRoot>
+    <Updater/>
     <Todo id={1}/>
     <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={2} />
   </RecoilRoot>
+}
+
+function Updater(){
+  const setUpdate = useSetRecoilState(todosAtomFamily(2));
+  setTimeout(()=>{
+    setUpdate({
+      title:"changedTitle",
+      description:"changedDesc"
+    })
+  },5000);
 }
 
 function Todo({id}) {
